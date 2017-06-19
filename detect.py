c094d2658d9d1829d9f001a79a2e8dab3089fde6
Kotaro Abe <abekoh.bcky@gmail.com>
2017-06-20 01:48:36 +0900

    refact

--- detect.py
+++ detect.py
@@ -1,50 +1,65 @@
 # -*- coding: utf-8 -*-
 import cv2
-import sys
+import os
+
 
 class Video:
+
     def __init__(self, video_path):
         self.video_cap = cv2.VideoCapture(video_path)
+        self.frame_i = 0
 
     def get(self):
         ret, frame = self.video_cap.read()
-        return frame
+        self.frame_i += 1
+        self._skip()
+        if ret:
+            return frame, self.frame_i
+        return None, self.frame_i
+
+    def _skip(self, count_n=24 * 5):
+        count_i, ret = 0, True
+        while ret and count_i < count_n:
+            ret, _ = self.video_cap.read()
+            self.frame_i += 1
+            count_i += 1
 
 
-def detect(img, cascade_path='./lbpcascade_animeface.xml'):
-    cascade = cv2.CascadeClassifier(cascade_path)
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray_img = cv2.equalizeHist(gray_img)
+class Detector:
 
-    face_ranges = cascade.detectMultiScale(gray_img, scaleFactor = 1.1, minNeighbors = 5, minSize = (24, 24))
+    def __init__(self, video_path, min_size=(128, 128), cascade_path='./lbpcascade_animeface.xml', output_dir_path='output'):
+        self.video = Video(video_path)
+        self.min_size = min_size
+        self.cascade_path = cascade_path
+        self.output_dir_path = output_dir_path
+        self.output_video_dir_path = os.path.join(output_dir_path, os.path.basename(video_path))
+        if not os.path.exists(self.output_video_dir_path):
+            os.makedirs(self.output_video_dir_path)
 
-    face_imgs = []
+    def get_faces_one_frame(self, img):
+        cascade = cv2.CascadeClassifier(self.cascade_path)
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray_img = cv2.equalizeHist(gray_img)
+        face_ranges = cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5, minSize=self.min_size)
+        face_imgs = []
+        for x, y, w, h in face_ranges:
+            face_img = img[y:y + h, x:x + w]
+            face_imgs.append(face_img)
+        return face_imgs
 
-    for (x, y, w, h) in face_ranges:
-        face_img = img[y : y + h, x : x + w]
-        face_imgs.append(face_img)
-        # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    def save_img(self, img, frame_i, face_i):
+        cv2.imwrite(os.path.join(self.output_video_dir_path, '{}_{}.png'.format(frame_i, face_i)), img)
 
-    # cv2.imshow('result', img)
-    # cv2.waitKey(0)
-    # cv2.imwrite('result.png', img)
-    return face_imgs
+    def save_faces(self):
+        while 1:
+            frame, frame_i = self.video.get()
+            if frame is None:
+                break
+            face_imgs = self.get_faces_one_frame(frame)
+            for face_i, face_img in enumerate(face_imgs):
+                self.save_img(face_img, frame_i, face_i)
 
-def detect_video(video_path):
-    src_video = Video(video_path)
-    num = 0
-    while 1:
-        frame = src_video.get()
-        face_imgs = detect(frame)
-        for face_img in face_imgs:
-            cv2.imwrite('pic/' + str(num) + '.png', face_img)
-            num += 1
-
-
-def debug():
-    # img = cv2.imread('./cp.jpg')
-    # detect(img)
-    detect_video('./newgame_2.mp4')
 
 if __name__ == '__main__':
-    debug()
+    detector = Detector('./eupho1_1.mp4')
+    detector.save_faces()
