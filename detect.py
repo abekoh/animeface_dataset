a2c079d5838621af72bc2705d73701677f1ab7a4
Kotaro Abe <abekoh.bcky@gmail.com>
2017-06-23 20:30:00 +0900

    update

--- detect.py
+++ detect.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import cv2
 import os
 
@@ -11,13 +10,12 @@ class Video:
 
     def get(self):
         ret, frame = self.video_cap.read()
-        self.frame_i += 1
         self._skip()
         if ret:
             return frame, self.frame_i
         return None, self.frame_i
 
-    def _skip(self, count_n=24 * 5):
+    def _skip(self, count_n=24):
         count_i, ret = 0, True
         while ret and count_i < count_n:
             ret, _ = self.video_cap.read()
@@ -27,16 +25,51 @@ class Video:
 
 class Detector:
 
-    def __init__(self, video_path, min_size=(128, 128), cascade_path='./lbpcascade_animeface.xml', output_dir_path='output'):
-        self.video = Video(video_path)
+    def __init__(self, src_dir_path, dst_dir_path='dst', min_size=(256, 256), resized_size=(256, 256), cascade_path='./lbpcascade_animeface.xml', video_exts=['.mp4']):
+        self.src_dir_path = os.path.normpath(src_dir_path)
+        self.dst_dir_path = os.path.normpath(dst_dir_path)
         self.min_size = min_size
+        self.resized_size = resized_size
         self.cascade_path = cascade_path
-        self.output_dir_path = output_dir_path
-        self.output_video_dir_path = os.path.join(output_dir_path, os.path.basename(video_path))
-        if not os.path.exists(self.output_video_dir_path):
-            os.makedirs(self.output_video_dir_path)
+        self.video_exts = video_exts
+        self.video_paths = []
+        self._get_video_paths()
 
-    def get_faces_one_frame(self, img):
+    def _get_video_paths(self):
+        for dirpath, dirnames, filenames in os.walk(self.src_dir_path):
+            for filename in filenames:
+                filepath = os.path.join(dirpath, filename)
+                if os.path.splitext(filepath)[1] in self.video_exts:
+                    self.video_paths.append(filepath)
+
+    def detect(self):
+        for video_path in self.video_paths:
+            print('detecting faces in {}'.format(video_path))
+            dst_video_dir_path = self._get_dst_video_dir_path(video_path)
+            video = Video(video_path)
+            while 1:
+                img, frame_i = video.get()
+                if img is None:
+                    break
+                face_imgs = self._get_faces_one_frame(img)
+                if len(face_imgs) > 0:
+                    print('detect {} faces at {} frame'.format(len(face_imgs), frame_i))
+                    for face_i, face_img in enumerate(face_imgs):
+                        resized_face_img = self._resize_img(face_img, self.min_size)
+                        cv2.imwrite(os.path.join(dst_video_dir_path, '{}_{}.png'.format(frame_i, face_i)), resized_face_img)
+
+    def _get_dst_video_dir_path(self, video_path):
+        if os.path.split(video_path)[0] == self.src_dir_path:
+            dst_video_dir_path = self.dst_dir_path
+        else:
+            dirname = video_path.split('/')[-2]
+            filename = video_path.split('/')[-1].split('.')[0]
+            dst_video_dir_path = os.path.join(self.dst_dir_path, dirname, filename)
+        if not os.path.exists(dst_video_dir_path):
+            os.makedirs(dst_video_dir_path)
+        return dst_video_dir_path
+
+    def _get_faces_one_frame(self, img):
         cascade = cv2.CascadeClassifier(self.cascade_path)
         gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         gray_img = cv2.equalizeHist(gray_img)
@@ -47,19 +80,10 @@ class Detector:
             face_imgs.append(face_img)
         return face_imgs
 
-    def save_img(self, img, frame_i, face_i):
-        cv2.imwrite(os.path.join(self.output_video_dir_path, '{}_{}.png'.format(frame_i, face_i)), img)
-
-    def save_faces(self):
-        while 1:
-            frame, frame_i = self.video.get()
-            if frame is None:
-                break
-            face_imgs = self.get_faces_one_frame(frame)
-            for face_i, face_img in enumerate(face_imgs):
-                self.save_img(face_img, frame_i, face_i)
+    def _resize_img(self, img, size):
+        return cv2.resize(img, size)
 
 
 if __name__ == '__main__':
-    detector = Detector('./eupho1_1.mp4')
-    detector.save_faces()
+    detector = Detector('src')
+    detector.detect()
